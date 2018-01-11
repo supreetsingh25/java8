87b2c0699a833776ca71314bdd9e6bda46ae0e85
Supreet Singh <supreetsingh25@gmail.com>
2018-01-11 18:45:00 +0530

    Hello

--- Java8Features/src/com/ashok/java8/functionalinterface/LambdaExpression.java
+++ Java8Features/src/com/ashok/java8/functionalinterface/LambdaExpression.java
@@ -20,18 +20,18 @@ public class LambdaExpression {
 	public static void main(String[] args) {
 		
 		A obj;
-		//obj=new Xyz();
+		//=new Xyz();
 		/*obj=new A() {
 			public void show() {
 				System.out.println("Hello");
 			}
-		};*/
+		};
+		*/
+		obj=()->{
+			System.out.println("Hello");
+		};
 		
-		/*obj=()->{
-			System.out.println("hello");
-		};*/
-		
-		obj=()->System.out.println("hello");
+	//	obj=()->System.out.println("hello");
 		
 		obj.show();
 		
